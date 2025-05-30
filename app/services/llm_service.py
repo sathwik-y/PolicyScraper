b11982231c577c75a4e345b6cb98f785edea8e0b
sathwik-y <sathwik.yellapragada@gmail.com>
2025-05-30 15:45:10 +0530

    Clean up

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -10,14 +10,6 @@ client = anthropic.Anthropic(api_key=CLAUDE_API_KEY)
 def extract_line_numbers(markdown_text):
     """Extract the line numbers from the markdown"""
     try:
-        # prompt = f"""
-        # You are tasked with extracting line numbers of a given markdown content. 
-        # The markdown content is converted from html page. 
-        # The purpose of the extraction is to include only necessary to upload to the
-        # knowledge base of the company and omitting irrelevant information for the purpose 
-        # of the page
-        # {markdown_text}
-        # """
         prompt = f"""
         You are an AI assistant tasked to extract only the necessary content from a markdown document converted from HTML.
         The markdown contains line numbers appended as 'LINE_NUMBER: content'.
--- app/services/scraper.py
+++ app/services/scraper.py
@@ -2,28 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_html_from_url(url: str) -> str:
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    #     'Accept-Language': 'en-US,en;q=0.5',
-    #     'Accept-Encoding': 'gzip, deflate',
-    #     'Connection': 'keep-alive',
-    #     'Upgrade-Insecure-Requests': '1',
-    # }
-    response = requests.get(url, headers=headers, timeout=10)
+    response = requests.get(url,timeout=10)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "html.parser")
 
     return str(soup) 
 
-# from requests_html import HTMLSession
-
-# def get_html_from_url(url: str) -> str:
-#     session = HTMLSession()
-#     try:
-#         r = session.get(url)
-#         r.html.render(timeout=20)  
-#     except Exception as e:
-#         r = session.get(url)
-#         return r.html.html
